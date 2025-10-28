e1b64daedba92dbd229b1286e6024b9672427830
Mario <mario.m.g.5557@gmail.com>
2025-10-28 10:02:00 +0100

    HiloSumasRestas

--- UT2/Hilos1/HiloParInpar.java
+++ UT2/Hilos1/HiloParInpar.java
@@ -1,10 +1,10 @@
 public class HiloParInpar implements Runnable {
-   int type;
-   public HiloParInpar(int type) {
-      this.type = type;
-   }
+    int type;
+    public HiloParInpar(int type) {
+        this.type = type;
+    }
 
-   public void run() {
+    public void run() {
         if (this.type == 1) {
             for(int i = 2; i <= 100; i += 2) {
                 System.out.println("HILO xx generando número par: " + i);
@@ -15,11 +15,13 @@ public class HiloParInpar implements Runnable {
                 System.out.println("HILO yy generando número impar: " + i);
             }
         }
-
     }
 
-   public static void main(String[] args) {
-      (new Thread(new HiloParInpar(1))).start();
-      (new Thread(new HiloParInpar(2))).start();
-   }
+    public static void main(String[] args) throws InterruptedException {
+        Thread hiloImpares = new Thread(new HiloParInpar(2));
+        Thread hilopares = new Thread(new HiloParInpar(1));
+        hiloImpares.start();
+        hiloImpares.join();
+        hilopares.start();
+    }
 }
---
+++ UT2/Hilos1/HiloSumasRestas.java
@@ -0,0 +1,30 @@
+public class HiloSumasRestas implements Runnable {
+    private int num, numveces;
+    private String op;
+
+    public HiloSumasRestas (int numveces, String operacion){
+        this.num = 1000;
+        this.numveces = numveces;
+        this.op = operacion;
+    }
+
+    public void run() {
+        if (op.equals("+")) incrementar(numveces);
+        if (op.equals("-")) decrementar(numveces);
+        System.out.println("Hilo " + Thread.currentThread().getName() + " total: " + num);
+    }
+
+    public void incrementar (int numveces){num += numveces;}
+    public void decrementar (int numveces){num -= numveces;}
+
+    public static void main(String args[]) {
+        HiloSumasRestas hiloSuma1 = new HiloSumasRestas(100, "+");
+        HiloSumasRestas hiloResta2 = new HiloSumasRestas(100, "-");
+        HiloSumasRestas hiloSuma3 = new HiloSumasRestas(1, "+");
+        HiloSumasRestas hiloResta4 = new HiloSumasRestas(1, "-");
+        new Thread(hiloSuma1, "hiloSuma1").start();
+        new Thread(hiloResta2, "hiloResta2").start();
+        new Thread(hiloSuma3, "hiloSuma3").start();
+        new Thread(hiloResta4, "hiloResta4").start();
+    }
+}
